f50d4cdd8ab3005d044b72b1d109ddb906d2f6f8
Jessica Biondi <jbiondi.cs@gmail.com>
2023-10-10 15:28:55 -0400

    root route, register route, register form

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Flask app for Notes"""
 import os
-from flask import Flask, request, jsonify, render_template
-from models import db, connect_db
+from flask import Flask, request, jsonify, render_template, redirect, flash
+from models import db, connect_db, User
+from forms import RegisterUserForm
 
 
 app = Flask(__name__)
@@ -14,3 +15,42 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+
+@app.get('/')
+def homepage():
+    ''' redirect user to /register '''
+
+    return redirect('/register')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    ''' shows and handles user registration form '''
+
+    form = RegisterUserForm()
+
+    if form.validate_on_submit():
+
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f'Successfully added {new_user.name}')
+        return redirect(f'/users/{new_user.username}')
+
+    else:
+        return render_template('register.html', form=form)
---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired, Email
+
+
+class RegisterUserForm(FlaskForm):
+    ''' Form for registering new notes app users '''
+
+    username = StringField('User Name', validators=[InputRequired()])
+
+    password = StringField('Password', validators=[InputRequired()])
+
+    email = StringField('Email', validators=[InputRequired(), Email()])
+
+    first_name = StringField('First Name', validators=[InputRequired()])
+
+    last_name = StringField('Last Name', validators=[InputRequired()])
